d427870c0ed74ef6c09b7b26c74e3042e8bc7a2b
bartolu3 <bartolu3@J08-18.uhk.cz>
2016-11-02 13:07:46 +0100

    dodelani hraci plochy

--- src/hra/HraciPlocha.java
+++ src/hra/HraciPlocha.java
@@ -1,6 +1,8 @@
 package hra;
 
-public class HraciPlocha {
+import javax.swing.JPanel;
+
+public class HraciPlocha extends JPanel{
 	public static final int VYSKA = 800;
 	public static final int SIRKA = 600;
 }
